3096808a6395e2803b16cc2428f6d5f7cfc9c630
tobezero01 <ducnhuad@gmail.com>
2024-03-14 11:14:49 +0700

    contact listview and sqlite

--- app/src/main/java/com/example/testexam/SinhVienAdapter.java
+++ app/src/main/java/com/example/testexam/SinhVienAdapter.java
@@ -1,5 +1,47 @@
 package com.example.testexam;
 
-public class SinhVienAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class SinhVienAdapter extends ArrayAdapter<SinhVien> {
+
+    public SinhVienAdapter(@NonNull Context context, int resource, int textViewResourceId) {
+        super(context, resource, textViewResourceId);
+    }
+
+    public SinhVienAdapter(@NonNull Context context, int resource, @NonNull List<SinhVien> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.sinhvien, parent, false);
+        }
+
+        // Lấy đối tượng SinhVien tại vị trí position
+        SinhVien sinhVien = getItem(position);
+
+        // Tìm và gán dữ liệu vào các TextView trong layout sinhvien.xml
+        TextView textViewName = listItemView.findViewById(R.id.textViewName);
+        TextView textViewYear = listItemView.findViewById(R.id.textViewYear);
+        TextView textViewMark = listItemView.findViewById(R.id.textViewMark);
+
+        textViewName.setText("Name: " + sinhVien.getName());
+        textViewYear.setText("Year of Birth: " + sinhVien.getYearOfBirth());
+        textViewMark.setText("Mark: " + sinhVien.getMark());
+
+        return listItemView;
+    }
 }
